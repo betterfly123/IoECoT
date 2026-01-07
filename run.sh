66b3a40cf1352ab70a7eb60a609f732b8be64525
RandomDream <51893117+betterfly123@users.noreply.github.com>
2026-01-07 22:03:41 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,18 @@
+# API
 python main.py \
-    --model_name 'mixtral-8x7b-32768' \
-    --data_name 'MELD' \
-    --data_path '' \
-    --task_type 'ERC'\
-    --method_type 'ioecot' \
-    --default_emo 5
+  --model_name "mixtral-8x7b-32768" \
+  --data_name "MELD" \
+  --data_path "/path/to/datasets_root" \
+  --task_type "ERC" \
+  --method_type "ioecot" \
+  --default_emo 5
+
+# Local
+python main.py \
+  --model_name "chatglm" \
+  --local_model_path "/path/to/your/chatglm" \
+  --data_name "MELD" \
+  --data_path "/path/to/datasets_root" \
+  --task_type "ERC" \
+  --method_type "ioecot" \
+  --default_emo 5
